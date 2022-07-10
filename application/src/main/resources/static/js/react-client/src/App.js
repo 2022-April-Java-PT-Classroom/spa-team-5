49d4137f0ab1500ba30e120418e8119313b1b2a0
scald97 <serena.ann.caldarella@gmail.com>
2022-07-10 16:32:59 -0400

    files and color scheme

--- application/src/main/resources/static/js/react-client/src/App.js
+++ application/src/main/resources/static/js/react-client/src/App.js
@@ -1,11 +1,8 @@
-import React from 'react';
+import React from "react";
+import style from "./style.module.scss";
 
 const App = () => {
-  return (
-    <div>
-      Hello React World!
-    </div>
-  );
-}
+  return <div>Hello React World!</div>;
+};
 
 export default App;
