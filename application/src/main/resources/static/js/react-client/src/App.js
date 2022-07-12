7d43157b4cdc7f611b4db4f19644393d6e678d5a
samleivadev <samdleiva@gmail.com>
2022-07-11 23:22:46 -0400

    Routing Pages, all routes for pages, header and footer are working good

--- application/src/main/resources/static/js/react-client/src/App.js
+++ application/src/main/resources/static/js/react-client/src/App.js
@@ -1,8 +1,20 @@
+import { BrowserRouter } from "react-router-dom";
+import Footer from "./components/footer";
+import Header from "./components/header";
+import PageSwitch from "./components/app-router";
 import React from "react";
 import style from "./style.module.scss";
 
 const App = () => {
-  return <div>Hello React World!</div>;
+  return (
+    <BrowserRouter>
+      <Header/>
+      <div className={style.page}>
+      <PageSwitch />
+      <Footer/>
+      </div>
+    </BrowserRouter>
+  )
 };
 
 export default App;
---
+++ application/src/main/resources/static/js/react-client/src/pages/books-screen/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BooksScreen = () => {
+    return (
+        <div>
+            <h2>This is the Favorite Books Page</h2>
+        </div>
+    )
+}
+
+export default BooksScreen;
