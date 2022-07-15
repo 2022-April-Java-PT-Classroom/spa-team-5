e5bb612a803c1c8e5e1a0fe4e90553218537ea64
ackley787 <ackley787@gmail.com>
2022-07-14 21:17:51 -0400

    started updating bio page cant get the grid layout to work. and added assets folder

--- application/src/main/resources/static/js/react-client/src/pages/bio-screen/index.js
+++ application/src/main/resources/static/js/react-client/src/pages/bio-screen/index.js
@@ -1,11 +1,148 @@
+import Adam from '../../assets/Adam.jpg';
 import React from "react";
+import style from './style.module.scss';
 
 const BioScreen = () => {
     return (
-        <div>
+        <div className="cards" >
             <h2>This is the Bio Screen Page</h2>
-        </div>
-    )
+       
+            <div className={style.page} >
+       
+            <div className="card" >
+            <img src={Adam} alt='Adam profile pic' />
+            
+            <p>
+                
+            I am currently enrolled at We Can Code IT 
+            to earn a Certificate in Software Development.
+            It is my goal to become a Full-Stack Developer. 
+            I have a passion for coding and enjoy working with 
+            others in a team setting as well as independently. 
+            I am an efficient problem solver with excellent 
+            troubleshooting skills. I am flexible and always 
+            seeking to acquire new skills. Besides having 
+            earned an Associates degree in Network Systems 
+            Administration. I am an avid computer gamer and 
+            have great interest in computer hardware and remote 
+            control cars.
+                
+            </p>
+            </div>
+                 
+            <div className="card" >
+            <img src={Adam} alt='2nd profile pic' />
+            
+            <p>
+                
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Cras eleifend ultricies turpis a dignissim. Donec sit 
+            amet accumsan orci. Fusce et magna facilisis nibh vestibulum 
+            pretium vitae id augue. Etiam at viverra orci. Praesent rutrum
+            purus nec faucibus varius. Praesent est erat, rutrum eget
+            dictum vitae, mattis eget leo. Integer dictum ultrices blandit.
+            Aliquam sed ipsum viverra, molestie metus quis, elementum ex.
+            Aenean scelerisque sodales bibendum. Aliquam rutrum euismod
+            eros ut pellentesque. Duis scelerisque convallis sagittis. 
+            
+            </p>
+            </div>
+                 
+            <div className="card" >
+            <img src={Adam} alt='3rd profile pic' />
+            
+            <p>
+                    
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Cras eleifend ultricies turpis a dignissim. Donec sit 
+            amet accumsan orci. Fusce et magna facilisis nibh vestibulum 
+            pretium vitae id augue. Etiam at viverra orci. Praesent rutrum
+            purus nec faucibus varius. Praesent est erat, rutrum eget
+            dictum vitae, mattis eget leo. Integer dictum ultrices blandit.
+            Aliquam sed ipsum viverra, molestie metus quis, elementum ex.
+            Aenean scelerisque sodales bibendum. Aliquam rutrum euismod
+            eros ut pellentesque. Duis scelerisque convallis sagittis. 
+                
+            </p>
+            </div>
+            
+            <div className="card" >
+                
+            <img src={Adam} alt='4th profile pic' />
+            
+            <p>
+                
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Cras eleifend ultricies turpis a dignissim. Donec sit 
+            amet accumsan orci. Fusce et magna facilisis nibh vestibulum 
+            pretium vitae id augue. Etiam at viverra orci. Praesent rutrum
+            purus nec faucibus varius. Praesent est erat, rutrum eget
+            dictum vitae, mattis eget leo. Integer dictum ultrices blandit.
+            Aliquam sed ipsum viverra, molestie metus quis, elementum ex.
+            Aenean scelerisque sodales bibendum. Aliquam rutrum euismod
+            eros ut pellentesque. Duis scelerisque convallis sagittis. 
+            
+            </p>
+            </div>
+            
+            <div className="card" >
+            
+            <img src={Adam} alt='5th profile pic' />
+            
+            <p>
+                
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Cras eleifend ultricies turpis a dignissim. Donec sit 
+            amet accumsan orci. Fusce et magna facilisis nibh vestibulum 
+            pretium vitae id augue. Etiam at viverra orci. Praesent rutrum
+            purus nec faucibus varius. Praesent est erat, rutrum eget
+            dictum vitae, mattis eget leo. Integer dictum ultrices blandit.
+            Aliquam sed ipsum viverra, molestie metus quis, elementum ex.
+            Aenean scelerisque sodales bibendum. Aliquam rutrum euismod
+            eros ut pellentesque. Duis scelerisque convallis sagittis. 
+            
+            </p>
+            </div>
+            
+            <div className="card" >
+            
+            <img src={Adam} alt='6th profile pic' />
+            
+            <p>
+                
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Cras eleifend ultricies turpis a dignissim. Donec sit 
+            amet accumsan orci. Fusce et magna facilisis nibh vestibulum 
+            pretium vitae id augue. Etiam at viverra orci. Praesent rutrum
+            purus nec faucibus varius. Praesent est erat, rutrum eget
+            dictum vitae, mattis eget leo. Integer dictum ultrices blandit.
+            Aliquam sed ipsum viverra, molestie metus quis, elementum ex.
+            Aenean scelerisque sodales bibendum. Aliquam rutrum euismod
+            eros ut pellentesque. Duis scelerisque convallis sagittis. 
+            
+            </p>
+            </div>
+            
+            <img src={Adam} alt='7th profile pic' />
+            
+            <p>
+                
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Cras eleifend ultricies turpis a dignissim. Donec sit 
+            amet accumsan orci. Fusce et magna facilisis nibh vestibulum 
+            pretium vitae id augue. Etiam at viverra orci. Praesent rutrum
+            purus nec faucibus varius. Praesent est erat, rutrum eget
+            dictum vitae, mattis eget leo. Integer dictum ultrices blandit.
+            Aliquam sed ipsum viverra, molestie metus quis, elementum ex.
+            Aenean scelerisque sodales bibendum. Aliquam rutrum euismod
+            eros ut pellentesque. Duis scelerisque convallis sagittis. 
+            
+            </p>
+                 
+            
+            </div>
+            </div>
+        );
 }
 
 export default BioScreen;
