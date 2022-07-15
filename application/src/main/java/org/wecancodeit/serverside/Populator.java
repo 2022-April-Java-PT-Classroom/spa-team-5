98d2ebe528c5650ef673329842b7552979cbed9a
joebuck125 <joe.jmoney@gmail.com>
2022-07-15 15:34:05 -0400

    scores api

---
+++ application/src/main/java/org/wecancodeit/serverside/Populator.java
@@ -0,0 +1,29 @@
+package org.wecancodeit.serverside;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.wecancodeit.serverside.models.UserName;
+import org.wecancodeit.serverside.repositories.UserNameRepository;
+
+import javax.annotation.Resource;
+
+@Component
+public class Populator implements CommandLineRunner {
+
+    @Resource
+    private UserNameRepository userNameRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+
+        UserName joe = new UserName("Joe","America", 35542);
+        userNameRepository.save(joe);
+        UserName adam = new UserName("Adam","America", 43253);
+        userNameRepository.save(adam);
+        UserName sam = new UserName("Sam","America", 45232);
+        userNameRepository.save(sam);
+
+
+    }
+}
