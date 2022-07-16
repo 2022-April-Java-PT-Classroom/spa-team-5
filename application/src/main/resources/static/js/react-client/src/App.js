beb0e9ca0252d9ea43b9067d2834488fbed10ef8
scald97 <serena.ann.caldarella@gmail.com>
2022-07-15 23:59:04 -0400

    branch issue

--- application/src/main/resources/static/js/react-client/src/App.js
+++ application/src/main/resources/static/js/react-client/src/App.js
@@ -1,8 +1,12 @@
+import Fish from "./components/fish";
 import React from "react";
 import style from "./style.module.scss";
 
 const App = () => {
-  return <div>Hello React World!</div>;
+  return;
+  <div>
+    <Fish />
+  </div>;
 };
 
 export default App;
