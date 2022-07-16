ea96da9b03cddc7d3aa42f2bc0d0c446070fe845
scald97 <serena.ann.caldarella@gmail.com>
2022-07-16 16:11:22 -0400

    changes to fishApi

--- application/src/main/resources/static/js/react-client/src/App.js
+++ application/src/main/resources/static/js/react-client/src/App.js
@@ -1,4 +1,7 @@
+<<<<<<< HEAD
 
+=======
+>>>>>>> bc693cd (edit to draft)
 import { BrowserRouter } from "react-router-dom";
 import Footer from "./components/footer";
 import Header from "./components/header";
@@ -8,6 +11,7 @@ import style from "./style.module.scss";
 
 const App = () => {
   return (
+<<<<<<< HEAD
 
 
     <BrowserRouter>
@@ -20,6 +24,16 @@ const App = () => {
     </BrowserRouter>
   )
 
+=======
+    <BrowserRouter>
+      <Header />
+      <div className={style.page}>
+        <PageSwitch />
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
+>>>>>>> bc693cd (edit to draft)
 };
 
 export default App;
--- application/src/main/resources/static/js/react-client/src/components/fish/index.js
+++ application/src/main/resources/static/js/react-client/src/components/fish/index.js
@@ -1,26 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import axios from "axios";
 import style from "./style.module.scss";
 
-const Fish = () => {
-  console.log(fish);
-
-  const [fishState, setFishState] = useState({
-    "Species Name": "",
-    "Scientific Name": "",
-    "Image Gallery": {
-      src: "",
-      alt: "",
-    },
-  });
-
-  axios
-    .post("https://www.fishwatch.gov/api/species", userData)
-    .then((response) => {
-      console.log(response.status);
+function Fish() {
+  useEffect(() => {
+    axios.get("https://www.fishwatch.gov/api/species").then((response) => {
       console.log(response.data);
+
+      // const [speciesState, setSpeciesState] = useState({
+      //   SpeciesName: "",
+      //   ScientificName: "",
+      //   ImageGallery: {
+      //     src: "",
+      //     alt: "",
+      //   },
+      // });
+
+      // const handleChange = (e) => {
+      //   const value = e.target.value;
+      //   setSpeciesState({
+      //     ...speciesState,
+      //     [e.target.SpeciesName]: value,
+      //   });
+      // };
     });
-};
+  });
+}
 
 export default Fish;
