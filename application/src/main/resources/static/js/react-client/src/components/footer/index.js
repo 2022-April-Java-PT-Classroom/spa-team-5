900c4de9c2a68c54483f0c95a8717261c8a3f115
samleivadev <samdleiva@gmail.com>
2022-07-17 13:32:42 -0400

    Simple footer styling

--- application/src/main/resources/static/js/react-client/src/components/footer/index.js
+++ application/src/main/resources/static/js/react-client/src/components/footer/index.js
@@ -3,11 +3,16 @@ import style from './style.module.scss';
 
 const Footer = () => (
     <div className={style.footer}>
+        <div class="wave" id="wave1"></div>
+        <div class="wave" id="wave1"></div>
+        <div class="wave" id="wave1"></div>
+        <div class="wave" id="wave1"></div>
+                
     <small>&copy; WCCI 2022 Team 5</small>
-    <ul className={style.socialList}>
-        <li>GitHub</li>
-        <li>LinkedIn</li>
-    </ul>
+    {/* <ul className={style.socialList}>
+        <li><a href="github.io">GitHub</a></li>
+        <li><a href="www.linkedin.com">LinkedIn</a></li>
+    </ul> */}
     </div>
     )
 
