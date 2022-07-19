bee201adb557308fd46edd2cbffd797f113acfd8
samleivadev <samdleiva@gmail.com>
2022-07-19 01:03:30 -0400

    Header styling complete

--- application/src/main/resources/static/js/react-client/src/components/header/index.js
+++ application/src/main/resources/static/js/react-client/src/components/header/index.js
@@ -4,17 +4,20 @@ import style from './style.module.scss';
 
 const Header = () => (
  
-    <div className={style.Header}> 
+    <div className={style.Header}>
+      
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Audiowide|Sofia|Trirong"></link>
       <h1>JAC Enterprises</h1>
       <ul className={style.navList}>
           <NavLink to='/'>Home</NavLink>
           <NavLink to='/bios'>Bios</NavLink>
-          <NavLink to='/books'>Books</NavLink>
+          <NavLink to='/books'>Activities</NavLink>
           <NavLink to='/scores'>Scores</NavLink>
           <NavLink to='/contact'>Contact</NavLink>
       </ul>
+      
     </div> 
-
+     
 )
 
 
