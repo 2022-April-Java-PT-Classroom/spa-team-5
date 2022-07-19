87a4ec9d09e4ca5d4d01316c5381f13a2f415034
samleivadev <samdleiva@gmail.com>
2022-07-19 17:32:01 -0400

    wave effect needs to be fixed

--- application/src/main/resources/static/js/react-client/src/components/footer/index.js
+++ application/src/main/resources/static/js/react-client/src/components/footer/index.js
@@ -1,18 +1,21 @@
+import {faGithub, faReact} from '@fortawesome/free-brands-svg-icons'
+
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import React from "react";
 import style from './style.module.scss';
+import waves from '../../assets/wave.png'
 
 const Footer = () => (
     <div className={style.footer}>
-        <div class="wave" id="wave1"></div>
-        <div class="wave" id="wave1"></div>
-        <div class="wave" id="wave1"></div>
-        <div class="wave" id="wave1"></div>
-                
-    <small>&copy; WCCI 2022 Team 5</small>
-    {/* <ul className={style.socialList}>
-        <li><a href="github.io">GitHub</a></li>
-        <li><a href="www.linkedin.com">LinkedIn</a></li>
-    </ul> */}
+         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Audiowide|Sofia|Trirong"></link>
+        <div className={style.wave}>
+            <img src={waves} alt='waves'/>
+        </div>
+        <div className={style.footer2}>       
+            <small>&copy; WCCI 2022  <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon> Team 5  <FontAwesomeIcon icon={faReact}></FontAwesomeIcon></small>
+            
+        </div>
+        
     </div>
     )
 
