bea28fb021c921b47730266d6db05d3343be7b06
ackley787 <ackley787@gmail.com>
2022-07-20 14:20:02 -0400

    done

--- application/src/main/resources/static/js/react-client/src/pages/bio-screen/index.js
+++ application/src/main/resources/static/js/react-client/src/pages/bio-screen/index.js
@@ -1,8 +1,5 @@
-import { brands, regular, solid } from '@fortawesome/fontawesome-svg-core/import.macro'
-
 import Adam from '../../assets/Adam.jpg';
 import CM_Rimal from '../../assets/CM Rimal.jpg';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import JamieBarr from '../../assets/JamieBarr.jpg';
 import Joe from '../../assets/Joe.png';
 import React from "react";
@@ -19,22 +16,27 @@ const BioScreen = () => {
        
        
             <section className={style.row}>
+                
             <article className={style.card}>
-                <h3>Adam Ackley</h3>
-                <img className={style.card} src={Adam} alt='Adam profile pic' />
-                            
-            <FontAwesomeIcon className={style.icon} icon="fa brands fa-github-square" 
-            href="https://github.com/ackley787" />
-
-            <FontAwesomeIcon className={style.icon} icon="fa-regular fa-address-card" 
-            href="https://ackley787.github.io/" />
-
-            <FontAwesomeIcon className={style.icon} icon="fa brands fa-linkedin-square" 
-            href="https://linkedin.com/in/adam-ackley78" />
+             
             
-            {/* <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon> */}
-                 
-                 <p>
+            <h3>Adam Ackley</h3>
+                
+            <img className={style.card} src={Adam} alt='Adam profile pic' />
+                            
+            {/* <a className={style.icon}  
+            href="https://github.com/ackley787">GitHub</a>
+
+            <a className={style.icon} 
+            href="https://ackley787.github.io/">Portfolio</a>
+
+
+            <a className={style.icon} 
+            href="https://linkedin.com/in/adam-ackley78">Linkedin</a> */}
+
+            
+                             
+            <p className={style.blurb}>
                 
             I am currently enrolled at We Can Code IT 
             to earn a Certificate in Software Development.
@@ -53,19 +55,21 @@ const BioScreen = () => {
             </article>
                  
             <article className={style.card}>
+                
             <h3>Samuel Leiva</h3>
+            
             <img className={style.card} src={SamHeroPic} alt='2nd profile pic' />
             
-            <FontAwesomeIcon className={style.icon} icon="fa-brands fa-github-square" 
-            href="https://github.com/samleivadev" />
+            {/* <a className={style.icon}  
+            href="https://github.com/samleivadev">GitHub</a>
 
-            <FontAwesomeIcon className={style.icon} icon="fa-regular fa-address-card" 
-            href="https://samleivadev.github.io/profile/" />
+            <a className={style.icon}
+            href="https://samleivadev.github.io/profile/">Portfolio</a>
 
-            <FontAwesomeIcon className={style.icon} icon="fa fa-linkedin-square" 
-            href="https://www.linkedin.com/in/samuelleiva/" />
+            <a className={style.icon} 
+            href="https://www.linkedin.com/in/samuelleiva/">Linkedin</a> */}
             
-            <p>
+            <p className={style.blurb}>
                 
             Hello! My name is Samuel and I enjoy creating things
             that live on the internet. I am a Full-Stack Software 
@@ -89,10 +93,21 @@ const BioScreen = () => {
             </article>
                  
             <article className={style.card}>
+                
             <h3>Joe Buckley</h3>
+            
             <img className={style.card} src={Joe} alt='3rd profile pic' />
             
-            <p>
+             {/* <a className={style.icon}  
+            href="https://github.com/joebuck125">GitHub</a>
+
+            <a className={style.icon}
+            href="https://joebuck125.github.io/">Portfolio</a>
+
+            <a className={style.icon} 
+            href="https://www.linkedin.com/in/joe-buckley-4358561a2/">Linkedin</a> */}
+            
+            <p className={style.blurb}>
                     
             I have a passion and a mind for efficiency in the flow of commodities across
             the nation, and now for the flow of information across the web: from the actual
@@ -102,35 +117,45 @@ const BioScreen = () => {
             forward-facing, as it is vital and integral to our daily lives.
                 
             </p>
+            
             </article>
             
             <article className={style.card}>
+                
             <h3>Serena Howard</h3>
+            
             <img className={style.card} src={Serena} alt='4th profile pic' />
             
-            <p>
+            {/* <a className={style.icon} 
+            href="https://www.linkedin.com/in/serena-ann-howard">"Linkedin"</a>
+             */}
+            
+            <p className={style.blurb}>
                 
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Cras eleifend ultricies turpis a dignissim. Donec sit 
-            amet accumsan orci. Fusce et magna facilisis nibh vestibulum 
-            pretium vitae id augue. Etiam at viverra orci. Praesent rutrum
-            purus nec faucibus varius. Praesent est erat, rutrum eget
-            dictum vitae, mattis eget leo. Integer dictum ultrices blandit.
-            Aliquam sed ipsum viverra, molestie metus quis, elementum ex.
-            Aenean scelerisque sodales bibendum. Aliquam rutrum euismod
-            eros ut pellentesque. Duis scelerisque convallis sagittis. 
+            Highly skilled and experienced Customer Service/Support Specialist
+            with a demonstrated track record of success in the financial services
+            and auto insurance industries. Key strengths include Communication, 
+            Training, English, and Problem Solving. 
+
+            Currently attending We Can Code IT to become a full-stack Java developer 
+            to utilize my passion for development, desire to take on challenges, and 
+            continually learn and grow.
             
             </p>
+            
             </article>
+            
             </section>
             
             <section className={style.row}>
                 
             <article className={style.card}>
+                
             <h3>Kevin Pak</h3>
+            
             <img className={style.card} src={SithLord} alt='5th profile pic' />
             
-            <p>
+            <p className={style.blurb}>
                 
             Lorem ipsum dolor sit amet, consectetur adipiscing elit.
             Cras eleifend ultricies turpis a dignissim. Donec sit 
@@ -143,13 +168,16 @@ const BioScreen = () => {
             eros ut pellentesque. Duis scelerisque convallis sagittis. 
             
             </p>
+            
             </article>
             
             <article className={style.card}>
+                
             <h3>Jamie Barr</h3>
+            
             <img className={style.card} src={JamieBarr} alt='6th profile pic' />
             
-            <p>
+            <p className={style.blurb}>
                 
             Lorem ipsum dolor sit amet, consectetur adipiscing elit.
             Cras eleifend ultricies turpis a dignissim. Donec sit 
@@ -162,22 +190,26 @@ const BioScreen = () => {
             eros ut pellentesque. Duis scelerisque convallis sagittis. 
             
             </p>
+            
             </article>
             
             <article className={style.card}>
+                
             <h3>Chuda Rimal</h3>
+            
             <img className={style.card} src={CM_Rimal} alt='7th profile pic' />
-            <FontAwesomeIcon className={style.icon} icon="fa-brands fa-github-square" 
-            href="https://github.com/cmsrimsson" />
+            
+            {/* <a className={style.icon} 
+            href="https://github.com/cmsrimsson">GitHub</a>
 
-            <FontAwesomeIcon className={style.icon} icon="fa-regular fa-address-card" 
-            href="https://cmsrimsson.github.io/" />
+            <a className={style.icon} 
+            href="https://cmsrimsson.github.io/">Portfolio</a>
 
-            <FontAwesomeIcon className={style.icon} icon="fa fa-linkedin-square" 
-            href="https://www.linkedin.com/in/chuda-rimal-318b33201/" />
+            <a className={style.icon}
+            href="https://www.linkedin.com/in/chuda-rimal-318b33201/">Linkedin</a> */}
 
             
-            <p>
+            <p className={style.blurb}>
                 
             Hello, this is Chuda Rimal. I am a Full-Stack web developer and
             mostly I am passionate on web coding. I’m quite expert in using
@@ -194,7 +226,9 @@ const BioScreen = () => {
                  
             
             </article>
+            
             </section>
+            
          </div>
       
             
