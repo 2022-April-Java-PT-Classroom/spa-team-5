532f31c9ed70576038b61c1551274784261ba8ef
JamieCBarr <jamiecbarr@gmail.com>
2022-07-21 15:37:55 -0400

    game calls images from backend

--- application/src/main/resources/static/js/react-client/src/pages/home-screen/index.js
+++ application/src/main/resources/static/js/react-client/src/pages/home-screen/index.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import Axios from 'axios';
 import GameField from "../../components/gameField";
-import GameRow from "../../components/gameRow";
-import GameTile from "../../components/gameTile";
 import style from './homeStyle.module.scss';
 
 const HomeScreen = () => {
 
-return <GameField rows = {4} cols = {4}/>;
+   const [loadingTileImages, setLoadingTileImages] = useState(true);
+   const [tileImages, setTileImages] = useState(null);
+
+   useEffect(() => {
+    const fetchData = async () => {
+        const result = await Axios('http://localhost:8080/game-tiles/4');
+        setTileImages(result.data);
+    }
+    
+    if (tileImages) {
+        setLoadingTileImages(false);
+    }
+
+    
+        const timer = setTimeout(() => {
+            !tileImages && fetchData();
+        }, 1000);
+        return () => clearTimeout(timer);
+
+        // eslint-disable-next-line
+    }, [tileImages]);
+
+
+return <div>
+        {loadingTileImages ? <h3>Loading ...</h3> : <GameField rows = {4} cols = {4}/>}
+       </div>
 }
 
 export default HomeScreen;
