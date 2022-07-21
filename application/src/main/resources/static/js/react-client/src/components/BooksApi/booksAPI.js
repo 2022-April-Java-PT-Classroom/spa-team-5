da6ef5a7c6af89d33a4faf6c4d2bd1e3b844ba17
ackley787 <102875729+ackley787@users.noreply.github.com>
2022-07-20 20:05:33 -0400

    Revert "basic styling for book activities done"

--- application/src/main/resources/static/js/react-client/src/components/BooksApi/booksAPI.js
+++ application/src/main/resources/static/js/react-client/src/components/BooksApi/booksAPI.js
@@ -1,24 +1,22 @@
 import React, { useEffect, useState } from "react";
 
-import style from "./booksApiStyle.module.scss"
+import style from "./bookStyle.module.scss"
 
 export default function App() {
   const [collections, setCollections] = useState([]);
 
-  // useEffect(() => {
-  //   fetch("https://api.lib.harvard.edu/v2/collections.json")
-  //     .then((res) => res.json())
-  //     .then((data) => setCollections(data));
-  // }, []);
+  useEffect(() => {
+    fetch("https://api.lib.harvard.edu/v2/collections.json")
+      .then((res) => res.json())
+      .then((data) => setCollections(data));
+  }, []);
 
   return (
-    <body className={style.body}>
-    <div className={style.booksStyle}>
-      {/* <h1>Harvard Library Recomendations</h1> */}
-      {/* {collections.map((collection) => (
-        <div key={collection.systemId}>{collection.setName}</div> */}
-      {/* ))} */}
+    <div className={style.booksstyle}>
+      <h1>Harvard Library Recomendations</h1>
+      {collections.map((collection) => (
+        <div key={collection.systemId}>{collection.setName}</div>
+      ))}
     </div>
-    </body>
   );
 }
