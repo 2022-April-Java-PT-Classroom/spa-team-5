d11f63451cdcd670ce2c79726628bb9ecd4c858d
JamieCBarr <jamiecbarr@gmail.com>
2022-07-21 17:49:16 -0400

    clicking correct fish now starts a new round, clicking wrong fish ends game and goes to scores screen

--- application/src/main/resources/static/js/react-client/src/components/gameTile/index.js
+++ application/src/main/resources/static/js/react-client/src/components/gameTile/index.js
@@ -7,9 +7,13 @@ import RandomIntBetween from "../../util/randomIntBetween";
 const GameTile = (index, imageURL, isWinningTile) => {
     let tile;
     if (isWinningTile){
-        tile = <img key={index} src={altIMage} onClick={()=>console.log('Win!')}/>
+        tile = <img key={index} src={altIMage} onClick={()=>{
+            window.open('/');
+        }}/>
     } else {
-        tile = <img key={index} src={image} onClick={()=>console.log('Lose!')}/>
+        tile = <img key={index} src={image} onClick={()=>{
+            window.open('/scores');
+        }}/>
     }
 
     return tile;
